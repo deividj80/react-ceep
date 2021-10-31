3e17fc7faf564c128b51ca8fada3232650d92411
deividj80 <deividj80@gmail.com>
2021-10-30 21:33:08 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import { ListaDeNotas } from './components/ListaDeNotas'
-import { FormularioCadastro } from './components/FormularioCadastro'
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-function App() {
-  return (
-    <section>
-      <FormularioCadastro/>
-      <ListaDeNotas/>
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
-  );
+    );
+  }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
